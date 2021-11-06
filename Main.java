33e71304561f56a28a53ff69794687ac95dc3d3c
House-lovers7 <taisuke.got@gmail.com>
2021-11-07 07:53:40 +0900

    プラス習慣の修正

--- Main.java
+++ Main.java
@@ -1,24 +1,21 @@
 public class Main {
   public static void main(String[] args) {
     double good_habit_rate;
-    double bad_habit_rate;
+    double bad_habit_rate = 100;
+    double bad_habit_score = 100;
 
     for (int i = 1; i <= 365; i++) {
       good_habit_rate = 1.00 + i * 0.01;
-      for (int day = 0; day < 365; day++) {
-        double good_habit_score = 100;
-        good_habit_score = good_habit_score * good_habit_rate;
-        // System.out.println("毎日1%成長した結果は" + (int) good_habit_score);
-        System.out.println("毎日1%成長した結果" + day + "日後のScoreは" + (int) good_habit_score + "です");
-      }
+      double good_habit_score = 100;
+      good_habit_score = good_habit_score * good_habit_rate;
+      System.out.println("毎日1%成長した結果" + i + "日後のScoreは" + (int) good_habit_score + "です");
     }
     for (int j = 1; j <= 365; j++) {
-      bad_habit_rate = 1.00 - j * 0.01;
-      for (int day = 0; day < 365; day++) {
-        double bad_habit_score = 1000000;
-        bad_habit_score = bad_habit_score * bad_habit_rate;
-        System.out.println("毎日1%退化した結果" + day + "日後のScoreは" + (int) bad_habit_score + "です");
-      }
+      // bad_habit_rate = 1.00 - j * 0.01;
+      bad_habit_rate = (bad_habit_rate * 0.99) / 100;
+      bad_habit_score = bad_habit_score * bad_habit_rate;
+      System.out.println("毎日1%成長した結果" + j + "日後のScoreは" + (int) bad_habit_score + "です");
     }
+
   }
 }
