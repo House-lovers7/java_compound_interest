bb4a644342636794d40e0b8f4a071791a0fc3b36
House-lovers7 <taisuke.got@gmail.com>
2021-11-07 11:37:10 +0900

    365日後の結果のみ表示

--- Main.java
+++ Main.java
@@ -9,11 +9,15 @@ public class Main {
 
     for (int i = 1; i <= 365; i++) {
       good_habit_score = good_habit_score * (1 + good_habit_rate);
-      System.out.println("おめでとうございます!!毎日1%成長した結果" + i + "日後のScoreは" + (int) good_habit_score + "です");
+      if (i == 365) {
+        System.out.println("おめでとうございます!!毎日1%成長した結果" + i + "日後のScoreは" + (int) good_habit_score + "です");
+      }
     }
     for (int j = 1; j <= 365; j++) {
       bad_habit_score = bad_habit_score * (1 - bad_habit_rate);
-      System.out.println("お気をつけください!!毎日1%退化した結果" + j + "日後のScoreは" + (int) bad_habit_score + "です");
+      if (j == 365) {
+        System.out.println("お気をつけください!!毎日1%退化した結果" + j + "日後のScoreは" + (int) bad_habit_score + "です");
+      }
     }
   }
 }
