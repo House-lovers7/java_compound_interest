4344addd39813d1880e658be68575319f3cd3a37
House-lovers7 <taisuke.got@gmail.com>
2021-11-07 11:34:40 +0900

    習慣の複利、毎日表示完成

--- Main.java
+++ Main.java
@@ -1,21 +1,19 @@
 public class Main {
   public static void main(String[] args) {
-    double good_habit_rate;
-    double bad_habit_rate = 100;
+
+    double bad_habit_rate = 0.01;
+    double good_habit_score = 100;
+
+    double good_habit_rate = 0.01;
     double bad_habit_score = 100;
 
     for (int i = 1; i <= 365; i++) {
-      good_habit_rate = 1.00 + i * 0.01;
-      double good_habit_score = 100;
-      good_habit_score = good_habit_score * good_habit_rate;
-      System.out.println("毎日1%成長した結果" + i + "日後のScoreは" + (int) good_habit_score + "です");
+      good_habit_score = good_habit_score * (1 + good_habit_rate);
+      System.out.println("おめでとうございます!!毎日1%成長した結果" + i + "日後のScoreは" + (int) good_habit_score + "です");
     }
     for (int j = 1; j <= 365; j++) {
-      // bad_habit_rate = 1.00 - j * 0.01;
-      bad_habit_rate = (bad_habit_rate * 0.99) / 100;
-      bad_habit_score = bad_habit_score * bad_habit_rate;
-      System.out.println("毎日1%成長した結果" + j + "日後のScoreは" + (int) bad_habit_score + "です");
+      bad_habit_score = bad_habit_score * (1 - bad_habit_rate);
+      System.out.println("お気をつけください!!毎日1%退化した結果" + j + "日後のScoreは" + (int) bad_habit_score + "です");
     }
-
   }
 }
